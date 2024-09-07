03efd3a98dca6ef7408d5b09fdfcaf188248e5cb
GeorgeBronner <78767238+GeorgeBronner@users.noreply.github.com>
2024-09-07 10:04:30 -0400

    make db a .env property, pytest-async

--- database.py
+++ database.py
@@ -12,18 +12,18 @@ DB_USER = os.getenv('DB_USER', 'default_user')
 DB_PASSWORD = os.getenv('DB_PASSWORD', 'default_password')
 DB_HOST = os.getenv('DB_HOST', 'localhost')
 DB_PORT = os.getenv('DB_PORT', '5432')
-
-# use with sqlite
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./garmin.db"
-# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+USE_SQLITE_DB = os.getenv('USE_SQLITE_DB', 'false')
 
 # use with mysql
 # SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:password@10.9.8.221:3306/garminApplicationDatabase'
 # engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-# use with postgres
-SQLALCHEMY_DATABASE_URL = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/golfmapper2?options=-csearch_path%3Dmain'
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+if USE_SQLITE_DB == 'true' or USE_SQLITE_DB == 'True':
+    SQLALCHEMY_DATABASE_URL = "sqlite:///./garmin.db"
+    engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+else:
+    SQLALCHEMY_DATABASE_URL = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/golfmapper2?options=-csearch_path%3Dmain'
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
